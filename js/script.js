4e07fd5a978ed28e04e3078fb4cd5bb19cd2d7b6
oqsan-syan <osurenyan@bk.ru>
2020-11-01 13:13:06 +0400

    commited first block

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $(".scrollToTop").fadeIn(100);
+        } else {
+            $(".scrollToTop").fadeOut(100);
+        }
+    });
+
+    $(".scrollToTop").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+    });
+});
+
+
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const colorCh = document.querySelectorAll(".colorCh");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = () => {
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+};
+cancelBtn.onclick = () => {
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+};
+window.onscroll = () => {
+  if (this.scrollY >= 20) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
